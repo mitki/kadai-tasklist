48c3ebb73bc8bfa4001b704ac8b330737723a03f
mitsuki <apple.maroru03@gmail.com>
2020-09-11 07:36:11 +0000

    new create の記載

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
   get 'login' , to: 'sessions#new'
   post 'login' , to: 'sessions#create'
