05a8b56f432d0582cba5bc41e2c52f3935fb1da1
mitsuki <apple.maroru03@gmail.com>
2020-09-11 09:29:26 +0000

    細かな修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -44,6 +44,7 @@ class TasksController < ApplicationController
   end
   
   private
+  
   
   #Storong Parameter
   def task_params
